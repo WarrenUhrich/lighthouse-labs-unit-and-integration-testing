006b605f2540ecccfb4eb094d15019ee5acb0ab4
Warren <hello@warren.codes>
2023-02-14 10:00:56 -0700

    Remove unneeded code

--- moai-tree-axe/src/helpers.test.js
+++
@@ -1,8 +0,0 @@
-import { announceResult } from "./helpers";
-
-describe('pickOppositeTest', () => {
-    it('should return the opposite choice', () => {
-        const result = announceResult('moai', 'axe');
-        expect(result).toBe('Tied');
-    });
-});
