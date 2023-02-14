c16c07c3847d804283b25c8062a61eae817ece16
Warren <hello@warren.codes>
2023-02-14 11:53:53 -0700

    Add helper functions and test

--- README.md
+++ README.md
@@ -3,3 +3,33 @@
 * [ ] React Testing Tools
 * [ ] Adding Features and Tests
 * [ ] Coverage Reports
+
+## Types of Testing
+
+* Unit: each function (component) in isolation
+* Integration: multiple units (functions) working together
+* End-to-End: user story (full feature / app big-picture testing)
+* Static: linters (make sure the code is sound / valid)
+* Manual: running your code and seeing what happens!
+
+## Mocha and Chai
+
+* Mocha: CLI test runner (Node.js)
+* Chai: assertion library
+
+## React
+
+* Jest: Testing Framework (orginally intended for React)
+
+## Moai-Tree-Axe
+
+User Stories:
+
+* User to pick an option, the computer picks a random option (fair)
+* User to pick an option, the computer picks the winning option (cheat)
+
+3 helper functions:
+
+* Computer picks the opposite option
+* Computer picks a random option
+* Result (show a WIN, LOSS, or TIE)
---
+++ moai-tree-axe/src/helpers.test.js
@@ -0,0 +1,62 @@
+const { pickWinningOption, pickRandomOption, getResult } = require('./helpers');
+
+describe('pickWinningOption function', () => {
+
+    // 🗿 -> 🌳
+    // 🌳 -> 🪓
+    // 🪓 -> 🗿
+
+    it('returns 🌳 if given 🗿', () => {
+        const result = pickWinningOption('🗿');
+        expect(result).toBe('🌳');
+    });
+
+    it('returns 🪓 if given 🌳', () => {
+        const result = pickWinningOption('🌳');
+        expect(result).toBe('🪓');
+    });
+
+    it('returns 🗿 if given 🪓', () => {
+        const result = pickWinningOption('🪓');
+        expect(result).toBe('🗿');
+    });
+
+    // it('throws an error if given invalid input', () => {
+    //     const result = pickWinningOption('123');
+    //     // expect(result).toThrow(Error);
+    // }); 
+});
+
+describe('pickRandomOption function', () => {
+
+    it('returns one of 🗿,🌳,🪓', () => {
+        const result = pickRandomOption();
+        const options = ['🗿','🌳','🪓'];
+        const wasFound = options.includes(result);
+        expect(wasFound).toBe(true);
+    });
+
+});
+
+describe('getResult function', () => {
+
+    it('return a win message if the player choice beats the computer', () => {
+       const result = getResult('🗿', '🪓');
+       expect(result).toBe('YOU WIN!');
+    });
+
+    it('return a tie message if the player choice matches the computer', () => {
+        const result = getResult('🌳', '🌳');
+        expect(result).toBe('TIE GAME!');
+    });
+
+    it('return a lose message if the player choice loses to the computer', () => {
+       const result = getResult('🪓', '🗿');
+       expect(result).toBe('YOU LOSE!');
+    });
+
+    // it('throws an error if given invalid input', () => {
+    //     const result = getResult('123', '123');
+    //     expect(result).toThrow('Invalid emojis.');
+    // }); 
+});
