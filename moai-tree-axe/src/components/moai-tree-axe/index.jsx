807cf0609d1aeeb924c9a24e786d8cfa801f81ac
Warren Uhrich <hello@warren.codes>
2024-03-27 10:25:59 -0600

    Initial commit (for lecture)

--- moai-tree-axe/src/components/moai-tree-axe/index.jsx
+++ moai-tree-axe/src/components/moai-tree-axe/index.jsx
@@ -4,35 +4,12 @@ import Player from './Player';
 import Computer from './Computer';
 import Footer from './Footer';
 import {useEffect, useState} from 'react';
-import {pickWinningOption, pickRandomOption, getResult} from '../../helpers';
 
 export default function MoaiAxeTree() {
     const [playerChoice, setPlayerChoice] = useState({choice: undefined});
     const [computerChoice, setComputerChoice] = useState({choice: undefined});
     const [cheating, setCheating] = useState(false);
     const [result, setResult] = useState();
-
-    useEffect(() => {
-        if(playerChoice.choice && computerChoice.choice) {
-            const res = getResult(playerChoice.choice, computerChoice.choice);
-            setResult(res);
-        }
-    }, [playerChoice.choice, computerChoice.choice]);
-
-    useEffect(() => {
-        if(cheating) setComputerChoice(prev => {
-            return {
-                ...prev,
-                choice: pickWinningOption(playerChoice.choice)
-            }
-        });
-        else setComputerChoice(prev => {
-            return {
-                ...prev,
-                choice: pickRandomOption()
-            }
-        });
-    }, [playerChoice]);
 
     return (
         <main id="moai-tree-axe">
--- moai-tree-axe/src/helpers.js
+++ moai-tree-axe/src/helpers.js
@@ -1,55 +1,8 @@
-/**
- * Computer picks the opposite option
- * Computer picks a random option
- * Result (show a WIN, LOSS, or TIE)
- */
-
-// Cheating function! Whatever the player picks, we should pick the opposite...
 export const pickWinningOption = (playerChoice) => {
-    const validOptions = ['🗿', '🪓', '🌳'];
-    if(!validOptions.includes(playerChoice)) throw new Error('Invalid selection.');
-    
-    switch(playerChoice) {
-        case '🗿':
-            return '🌳';
-        case '🪓':
-            return '🗿';
-        case '🌳':
-            return '🪓';
-    }
 };
 
-// Should pick a valid choice of 🗿, 🪓, or 🌳
 export const pickRandomOption = () => {
-    const validOptions = ['🗿', '🪓', '🌳'];
-    const randomIndex = parseInt(Math.random() * 3);
-    const randomChoice = validOptions[randomIndex];
-
-    return randomChoice;
 };
 
-// Receive both player and PC choice...
-// Return text as to the outcome...
-// 1. Player Wins!
-// 2. Computer Wins!
-// 3. Tie!
 export const getResult = (playerChoice, computerChoice) => {
-    const validOptions = ['🗿', '🪓', '🌳'];
-
-    // Check if valid options are available.
-    if(!validOptions.includes(playerChoice)) throw Error('Invalid input.');
-    if(!validOptions.includes(computerChoice)) throw Error('Invalid input.');
-
-    // Tie:
-    if(playerChoice === computerChoice) return 'Tie game! 😲';
-
-    // Player Wins:
-    if(playerChoice === '🗿' && computerChoice === '🪓') return 'You win! 🎉';
-    if(playerChoice === '🪓' && computerChoice === '🌳') return 'You win! 🎉';
-    if(playerChoice === '🌳' && computerChoice === '🗿') return 'You win! 🎉';
-
-    // Computer Wins:
-    if(computerChoice === '🗿' && playerChoice === '🪓') return 'Computer wins! 💻';
-    if(computerChoice === '🪓' && playerChoice === '🌳') return 'Computer wins! 💻';
-    if(computerChoice === '🌳' && playerChoice === '🗿') return 'Computer wins! 💻';
 };
